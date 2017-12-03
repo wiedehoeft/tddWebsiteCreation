1a6747ef540c722df053f5a50331dfb751c5df7b
Christian Wiedehöft <wiedehoeft.christian@gmail.com>
2017-12-03 14:33:58 +0000

    first PoC for customElements event listener

--- src/js/PersonList.js
+++ src/js/PersonList.js
@@ -5,20 +5,35 @@ tmpl.innerHTML = `
   <slot></slot>
 `;*/
 
-let link = document.querySelector('link[rel="import"][href="PersonList.html"]');
-let template = link.import.querySelector('#person-list-tmpl');
-
 class PersonList extends HTMLElement {
 
     constructor() {
         super();
+        let link = document.querySelector('link[rel="import"][href="PersonList.html"]');
+        let template = link.import.querySelector('#person-list-tmpl');
         let shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        document.getElementById("buttonAddPerson").addEventListener("click", () => {
+            this.addPerson();
+        });
     }
 
-    connectedCallback() {
-        this.innerHTML = "<b>I'm an x-foo-with-markup!</b>";
+    addPerson(person) {
+        var tableRef = this.shadowRoot.getElementById('personTable').getElementsByTagName('tbody')[0];
+
+        // Insert a row in the table at the last row
+        var newRow = tableRef.insertRow(tableRef.rows.length);
+
+        // Insert a cell in the row at index 0
+        var newCell = newRow.insertCell(0);
+
+        // Append a text node to the cell
+        var newText = document.createTextNode('New row');
+        newCell.appendChild(newText);
     }
 }
 
 window.customElements.define("person-list", PersonList);
+
+module.exports.PersonList = PersonList;
